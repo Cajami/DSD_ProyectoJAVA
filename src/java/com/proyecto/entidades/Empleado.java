eb8f2c580edb1c5a3e34eb86cc65c86c6900f3fc
Javier Huiñocana <javier2315@gmail.com>
2017-07-08 01:10:29 -0500

    agregando la entidad Empleado

--- src/java/com/proyecto/entidades/Empleado.java
+++ src/java/com/proyecto/entidades/Empleado.java
@@ -6,31 +6,116 @@
 package com.proyecto.entidades;
 
 import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 /**
  *
  * @author USER
  */
-@Beat
 @Entity
+@Table(name = "EMPLEADO")
 public class Empleado implements Serializable {
 
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    @Column(name = "CODIGO_EMPLEADO")
+    private Long CODIGO_EMPLEADO;
+    private Long CODIGO_PAIS;
+    private Long CODIGO_TIPDOC;
+    private String NRO_DOC;
+    private String APELLIDO_PATERNO;
+    private String APELLIDO_MATERNO;
+    private String NOMBRES;
+    private Date FECHA_NACIMIENTO;
+    private String DOMICILIO;
 
-    public Long getId() {
-        return id;
+    public Long getCODIGO_EMPLEADO() {
+        return CODIGO_EMPLEADO;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public void setCODIGO_EMPLEADO(Long CODIGO_EMPLEADO) {
+        this.CODIGO_EMPLEADO = CODIGO_EMPLEADO;
     }
+
+    public Long getCODIGO_PAIS() {
+        return CODIGO_PAIS;
+    }
+
+    public void setCODIGO_PAIS(Long CODIGO_PAIS) {
+        this.CODIGO_PAIS = CODIGO_PAIS;
+    }
+
+    public Long getCODIGO_TIPDOC() {
+        return CODIGO_TIPDOC;
+    }
+
+    public void setCODIGO_TIPDOC(Long CODIGO_TIPDOC) {
+        this.CODIGO_TIPDOC = CODIGO_TIPDOC;
+    }
+
+    public String getNRO_DOC() {
+        return NRO_DOC;
+    }
+
+    public void setNRO_DOC(String NRO_DOC) {
+        this.NRO_DOC = NRO_DOC;
+    }
+
+    public String getAPELLIDO_PATERNO() {
+        return APELLIDO_PATERNO;
+    }
+
+    public void setAPELLIDO_PATERNO(String APELLIDO_PATERNO) {
+        this.APELLIDO_PATERNO = APELLIDO_PATERNO;
+    }
+
+    public String getAPELLIDO_MATERNO() {
+        return APELLIDO_MATERNO;
+    }
+
+    public void setAPELLIDO_MATERNO(String APELLIDO_MATERNO) {
+        this.APELLIDO_MATERNO = APELLIDO_MATERNO;
+    }
+
+    public String getNOMBRES() {
+        return NOMBRES;
+    }
+
+    public void setNOMBRES(String NOMBRES) {
+        this.NOMBRES = NOMBRES;
+    }
+
+    public Date getFECHA_NACIMIENTO() {
+        return FECHA_NACIMIENTO;
+    }
+
+    public void setFECHA_NACIMIENTO(Date FECHA_NACIMIENTO) {
+        this.FECHA_NACIMIENTO = FECHA_NACIMIENTO;
+    }
+
+    public String getDOMICILIO() {
+        return DOMICILIO;
+    }
+
+    public void setDOMICILIO(String DOMICILIO) {
+        this.DOMICILIO = DOMICILIO;
+    }
+
+    public String getEMAIL() {
+        return EMAIL;
+    }
+
+    public void setEMAIL(String EMAIL) {
+        this.EMAIL = EMAIL;
+    }
+    private String EMAIL;
 
     @Override
     public int hashCode() {
@@ -56,5 +141,5 @@ public class Empleado implements Serializable {
     public String toString() {
         return "com.proyecto.entidades.Empleado[ id=" + id + " ]";
     }
-    
+
 }
