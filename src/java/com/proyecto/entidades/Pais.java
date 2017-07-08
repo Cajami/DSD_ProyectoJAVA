fdb587ab3441187d119f4f90d58d7bc36a90d723
Javier Huiñocana <javier2315@gmail.com>
2017-07-08 01:24:19 -0500

    Agregando Entidades Pais y TipoDoc

--- src/java/com/proyecto/entidades/Pais.java
+++ src/java/com/proyecto/entidades/Pais.java
@@ -18,24 +18,16 @@ import javax.persistence.Table;
  * @author JAVIER-PC
  */
 @Entity
-@Table(name = "TIP_DOC")
-public class Tip_Doc implements Serializable {
+@Table(name = "PAIS")
+public class Pais implements Serializable {
 
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "CODITO_TIPDOC")
-    private Long CODITO_TIPDOC;
+    @Column(name = "CODIGO_PAIS")
+    private Long CODIGO_PAIS;
     @Column(name = "DESCRIPCION")
     private String DESCRIPCION;
-
-    public Long getCODITO_TIPDOC() {
-        return CODITO_TIPDOC;
-    }
-
-    public void setCODITO_TIPDOC(Long CODITO_TIPDOC) {
-        this.CODITO_TIPDOC = CODITO_TIPDOC;
-    }
 
     public String getDESCRIPCION() {
         return DESCRIPCION;
@@ -45,21 +37,29 @@ public class Tip_Doc implements Serializable {
         this.DESCRIPCION = DESCRIPCION;
     }
 
+    public Long getCODIGO_PAIS() {
+        return CODIGO_PAIS;
+    }
+
+    public void setCODIGO_PAIS(Long CODIGO_PAIS) {
+        this.CODIGO_PAIS = CODIGO_PAIS;
+    }
+
     @Override
     public int hashCode() {
         int hash = 0;
-        hash += (CODITO_TIPDOC != null ? CODITO_TIPDOC.hashCode() : 0);
+        hash += (CODIGO_PAIS != null ? CODIGO_PAIS.hashCode() : 0);
         return hash;
     }
 
     @Override
     public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof Tip_Doc)) {
+        // TODO: Warning - this method won't work in the case the CODIGO_PAIS fields are not set
+        if (!(object instanceof Pais)) {
             return false;
         }
-        Tip_Doc other = (Tip_Doc) object;
-        if ((this.CODITO_TIPDOC == null && other.CODITO_TIPDOC != null) || (this.CODITO_TIPDOC != null && !this.CODITO_TIPDOC.equals(other.CODITO_TIPDOC))) {
+        Pais other = (Pais) object;
+        if ((this.CODIGO_PAIS == null && other.CODIGO_PAIS != null) || (this.CODIGO_PAIS != null && !this.CODIGO_PAIS.equals(other.CODIGO_PAIS))) {
             return false;
         }
         return true;
@@ -67,7 +67,7 @@ public class Tip_Doc implements Serializable {
 
     @Override
     public String toString() {
-        return "com.proyecto.entidades.Tip_Doc[ id=" + CODITO_TIPDOC + " ]";
+        return "com.proyecto.entidades.Pais[ id=" + CODIGO_PAIS + " ]";
     }
 
 }
